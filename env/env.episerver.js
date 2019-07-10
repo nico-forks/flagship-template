5404d5148897a48a50f14aa2e87c509a6b482081
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2019-07-10 15:02:43 -0400

    chore: Remove development environment env

--- env/env.episerver.js
+++ env/env.episerver.js
@@ -1,13 +1,5 @@
 const common = require('./common');
 
 module.exports = {
-  ...common,
-  dataSource: {
-    type: 'episerver',
-    categoryFormat: 'list',
-    catalogName: 'Fashion',
-    apiConfig: {
-      apiHost: 'http://localhost:3000',
-    }
-  }
+  ...common
 };
